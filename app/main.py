cdb775e60772b6106b7c91a392d709adb649814d
Pavel Yasukevich <ptntbox@gmail.com>
2023-01-20 09:28:56 +0500

    postgres connection

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
+import os
+
+import psycopg
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from routers.users import router as UserRouter
 from routers.posts import router as PostRouter
+from routers.users import router as UserRouter
+
+load_dotenv()
+
+DBNAME = os.environ.get("DBNAME")
+DB_USER = os.environ.get("DB_USER")
 
 app = FastAPI()
-
 app.include_router(UserRouter)
 app.include_router(PostRouter)
 
@@ -12,3 +20,8 @@ app.include_router(PostRouter)
 @app.get("/")
 async def root():
     return {"FastApi workout": "Project to explore features of framework."}
+
+
+with psycopg.connect(f"dbname={DBNAME} user={DB_USER}") as conn:
+    with conn.cursor() as cur:
+        print(cur.execute("SELECT * FROM posts").fetchone())
